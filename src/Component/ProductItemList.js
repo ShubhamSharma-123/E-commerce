fec018442d91f024589522a70f79d295bf0409b7
Shubham Sharma <ss8680919@gmail.com>
2024-10-27 21:32:10 +0530

    last issue resolved

--- src/Component/ProductItemList.js
+++ src/Component/ProductItemList.js
@@ -1,10 +1,43 @@
+// import React from "react";
+// import ProductItem from "./ProductItem";
+// import { useSelector } from "react-redux";
+// import Sort from "./Sort";
+
+// export default function ProductItemList({}) {
+//   const data = useSelector((state) => state.products);
+//   if (data.length === 0) {
+//     return (
+//       <div className="d-flex justify-content-center mt-5">
+//         <div
+//           className="spinner-border"
+//           style={{ width: "8rem", height: "8rem" }}
+//           role="status"
+//         >
+//           <span className="visually-hidden">Loading...</span>
+//         </div>
+//       </div>
+//     );
+//   } else {
+//     return (
+//       <div className="d-flex flex-column container-sm mt-4">
+//         <Sort />
+//         {data.map((item) => (
+//           <ProductItem item={item} key={item.title} />
+//         ))}
+//       </div>
+//     );
+//   }
+// }
+
+// src/Component/ProductItemList.js
 import React from "react";
 import ProductItem from "./ProductItem";
 import { useSelector } from "react-redux";
 import Sort from "./Sort";
 
-export default function ProductItemList({}) {
-  const data = useSelector((state) => state.products);
+export default function ProductItemList({ products = [] }) {
+  const data = useSelector((state) => state.products); // Fetch products from Redux state
+
   if (data.length === 0) {
     return (
       <div className="d-flex justify-content-center mt-5">
@@ -17,16 +50,16 @@ export default function ProductItemList({}) {
         </div>
       </div>
     );
-  } else {
-    return (
-      <div className="d-flex flex-column container-sm mt-4">
-        <Sort />
-        {data.map((item) => (
-          <ProductItem item={item} key={item.title} />
-        ))}
-      </div>
-    );
   }
+
+  return (
+    <div className="d-flex flex-column container-sm mt-4">
+      <Sort />
+      {data.map((item) => (
+        <ProductItem item={item} key={item.id} />
+      ))}
+    </div>
+  );
 }
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,76 +1,140 @@
-import {
-    Add_products,
-    Add_cart,
-    Product_view,
-    Cart_items,
-    update_cart,
-    delete_cart
-  } from "../actions";
+// import {
+//     Add_products,
+//     Add_cart,
+//     Product_view,
+//     Cart_items,
+//     update_cart,
+//     delete_cart
+//   } from "../actions";
   
-  let initialState = {
-    products: [],
-    cart: [],
-    itemToDisplay: "",
-    totalCart: 0,
-  };
-  export default function products(state = initialState, actions) {
-    switch (actions.type) {
-      case Add_products:
-        return {
-          ...state,
-          products: actions.products,
-        };
+//   let initialState = {
+//     products: [],
+//     cart: [],
+//     itemToDisplay: "",
+//     totalCart: 0,
+//   };
+//   export default function products(state = initialState, actions) {
+//     switch (actions.type) {
+//       case Add_products:
+//         return {
+//           ...state,
+//           products: actions.products,
+//         };
        
-      case Add_cart:
-        let flag = state.cart.indexOf(actions.cart);
-        if (flag!==-1) {
-          actions.cart.qty += 1;
-          return {
-            ...state,
-          };
-        } else {
-          return {
-            ...state,
-            cart: [actions.cart, ...state.cart],
-          };
-        }
+//       case Add_cart:
+//         let flag = state.cart.indexOf(actions.cart);
+//         if (flag!==-1) {
+//           actions.cart.qty += 1;
+//           return {
+//             ...state,
+//           };
+//         } else {
+//           return {
+//             ...state,
+//             cart: [actions.cart, ...state.cart],
+//           };
+//         }
         
   
-      case Product_view:
-        return {
-          ...state,
-          itemToDisplay: actions.view,
-        };
-        break;
-      case Cart_items:
-        let { cart } = state;
-        let total = cart.reduce((total, item) => {
-          return (total += item.qty);
-        }, 0);
-        return {
-          ...state,
-          totalCart: total,
-        };
-        break;
-      case update_cart:
-        let index = state.cart.indexOf(actions.updatedItem);
-        let updatedCart = null;
-        if (index !== -1) {
-          state.cart[index] = actions.updatedItem;
-          updatedCart = state.cart;
-        }
-        return {
-          ...state,
-          cart: [...updatedCart],
-        };
-      case delete_cart:
-        let position = state.cart.indexOf(actions.item);
-        state.cart.splice(position, 1);
-        return {
-          ...state,
-        }
-      default:
-        return state;
-    }
+//       case Product_view:
+//         return {
+//           ...state,
+//           itemToDisplay: actions.view,
+//         };
+//         break;
+//       case Cart_items:
+//         let { cart } = state;
+//         let total = cart.reduce((total, item) => {
+//           return (total += item.qty);
+//         }, 0);
+//         return {
+//           ...state,
+//           totalCart: total,
+//         };
+//         break;
+//       case update_cart:
+//         let index = state.cart.indexOf(actions.updatedItem);
+//         let updatedCart = null;
+//         if (index !== -1) {
+//           state.cart[index] = actions.updatedItem;
+//           updatedCart = state.cart;
+//         }
+//         return {
+//           ...state,
+//           cart: [...updatedCart],
+//         };
+//       case delete_cart:
+//         let position = state.cart.indexOf(actions.item);
+//         state.cart.splice(position, 1);
+//         return {
+//           ...state,
+//         }
+//       default:
+//         return state;
+//     }
+//   }
+
+
+// src/reducers/index.js
+import {
+  Add_products,
+  Add_cart,
+  Product_view,
+  Cart_items,
+  update_cart,
+  delete_cart,
+} from "../actions";
+
+let initialState = {
+  products: [],
+  cart: [],
+  itemToDisplay: "",
+  totalCart: 0,
+};
+
+export default function products(state = initialState, action) {
+  switch (action.type) {
+    case Add_products:
+      return {
+        ...state,
+        products: action.products,
+      };
+
+    case Add_cart:
+      const exists = state.cart.find((item) => item.id === action.cart.id);
+      if (exists) {
+        const updatedCart = state.cart.map((item) =>
+          item.id === action.cart.id ? { ...item, qty: item.qty + 1 } : item
+        );
+        return { ...state, cart: updatedCart };
+      } else {
+        return { ...state, cart: [...state.cart, action.cart] };
+      }
+
+    case Product_view:
+      return {
+        ...state,
+        itemToDisplay: action.view,
+      };
+
+    case Cart_items:
+      const total = state.cart.reduce((acc, item) => acc + item.qty, 0);
+      return { ...state, totalCart: total };
+
+    case update_cart:
+      const updatedCart = state.cart.map((item) =>
+        item.id === action.updatedItem.id ? action.updatedItem : item
+      );
+      return { ...state, cart: updatedCart };
+
+    case delete_cart:
+      const filteredCart = state.cart.filter(
+        (item) => item.id !== action.item.id
+      );
+      return { ...state, cart: filteredCart };
+
+    default:
+      return state;
   }
+}
 
